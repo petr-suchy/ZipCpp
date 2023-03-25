760cae83dbfd8b889edc00b42e588bdcc3c77471
Petr Suchý <petrs.sokolo@seznam.cz>
2023-03-25 18:28:12 +0100

    Class InputArchiveStream removed.

--- include/ZipCpp/InputArchiveStream.h
+++ include/ZipCpp/InputArchiveStream.h
@@ -5,90 +5,54 @@
 
 namespace Zip {
 
+	// Creates an instance of input archive stream.
 	template<typename InputStream>
-	class InputArchiveStream : public Archive {
-	public:
+	Archive MakeInputArchive(InputStream inputStream)
+	{
+		return Archive(
+			[inputStream](int flags)
+			{
+				auto seekableSource = std::make_shared<
+					SeekableSourceStream<InputStream>
+				>(inputStream);
 
-		InputArchiveStream(InputStream inputStreamPtr) :
-			Archive(ZIP_RDONLY),
-			_inputStreamPtr(inputStreamPtr)
-		{}
+				Error error;
 
-	protected:
-
-		typedef SeekableSourceStream<
-			InputStream
-		> SeekableSource;
-
-		virtual ZipHandle::SharedPtr _openArchive(int flags)
-		{
-			auto seekableSource = std::make_shared<
-				SeekableSource
-			>(_inputStreamPtr);
-
-			_inputStreamPtr = nullptr;
-
-			Error error;
-
-			zip_source_t* zipSrcPtr = zip_source_function_create(
-				&SeekableSource::dispatch,
-				seekableSource.get(),
-				error.getInternalStructPtr()
-			);
-
-			if (!zipSrcPtr) {
-
-				throw std::runtime_error(
-					std::string("cannot create a zip archive source -> ")
-						+ error.getErrMessage()
+				zip_source_t* zipSrcPtr = zip_source_function_create(
+					&SeekableSourceStream<InputStream>::dispatch,
+					seekableSource.get(),
+					error.getInternalStructPtr()
 				);
 
-			}
+				if (!zipSrcPtr) {
 
-			zip_t* newZipPtr = zip_open_from_source(
-				zipSrcPtr,
-				flags,
-				error.getInternalStructPtr()
-			);
+					throw std::runtime_error(
+						"cannot create a zip archive source -> "
+							+ error.getErrMessage()
+					);
 
-			if (!newZipPtr) {
+				}
 
-				zip_source_free(zipSrcPtr);
-
-				throw std::runtime_error(
-					std::string("cannot open a zip archive from the data source -> ")
-						+ error.getErrMessage()
+				zip_t* newZipPtr = zip_open_from_source(
+					zipSrcPtr,
+					ZIP_RDONLY,
+					error.getInternalStructPtr()
 				);
 
+				if (!newZipPtr) {
+
+					zip_source_free(zipSrcPtr);
+
+					throw std::runtime_error(
+						"cannot open a zip archive from the data source -> "
+							+ error.getErrMessage()
+					);
+
+				}
+
+				return std::make_shared<ZipHandle>(newZipPtr, seekableSource);
 			}
-
-			return std::make_shared<ZipHandle>(newZipPtr, seekableSource);
-		}
-
-	private:
-
-		InputStream _inputStreamPtr;
-
-	};
-
-	// create an instance of input archive stream
-	template<typename InputStream>
-	static InputArchiveStream<
-		InputStream
-	> MakeInputArchive(InputStream is)
-	{
-		return InputArchiveStream<InputStream>(is);
-	}
-
-	// create a shared pointer to input archive stream
-	template<typename InputStream>
-	static std::shared_ptr<
-		InputArchiveStream<InputStream>
-	> MakeSharedInputArchive(InputStream is)
-	{
-		return std::make_shared<
-			InputArchiveStream<InputStream>
-		>(is);
+		);
 	}
 
 }
