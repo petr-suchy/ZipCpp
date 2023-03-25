2f46375a31621abd24b393f3d1d94eb0e3790435
Petr Suchý <petrs.sokolo@seznam.cz>
2023-03-25 19:12:19 +0100

    Class OutputArchiveStream removed.

--- include/ZipCpp/OutputArchiveStream.h
+++ include/ZipCpp/OutputArchiveStream.h
@@ -6,91 +6,60 @@
 
 namespace Zip {
 
+	// Create an instance of output archive stream.
 	template<typename OutputStream>
-	class OutputArchiveStream : public Archive {
-	public:
+	Archive MakeOutputArchive(OutputStream outputStream)
+	{
+		return Archive(
+			[outputStream](int flags)
+			{
+				auto writableSource = std::make_shared<
+					WritableSourceStream<
+						NullInputStream::SharedPtr,
+						OutputStream
+					>
+				>(std::make_shared<NullInputStream>(), outputStream);
 
-		OutputArchiveStream(OutputStream outputStreamPtr) :
-			Archive(ZIP_CREATE),
-			_outputStreamPtr(outputStreamPtr)
-		{}
+				Error error;
 
-	protected:
-
-		typedef WritableSourceStream<
-			NullInputStream::SharedPtr,
-			OutputStream
-		> WritableSource;
-
-		virtual ZipHandle::SharedPtr _openArchive(int flags)
-		{
-			auto writableSource = std::make_shared<
-				WritableSource
-			>(std::make_shared<NullInputStream>(), _outputStreamPtr);
-
-			_outputStreamPtr = nullptr;
-
-			Error error;
-
-			zip_source_t* zipSrcPtr = zip_source_function_create(
-				&WritableSource::dispatch,
-				writableSource.get(),
-				error.getInternalStructPtr()
-			);
-
-			if (!zipSrcPtr) {
-
-				throw std::runtime_error(
-					std::string("cannot create a zip archive source -> ")
-						+ error.getErrMessage()
+				zip_source_t* zipSrcPtr = zip_source_function_create(
+					&WritableSourceStream<
+						NullInputStream::SharedPtr,
+						OutputStream
+					>::dispatch,
+					writableSource.get(),
+					error.getInternalStructPtr()
 				);
 
-			}
+				if (!zipSrcPtr) {
 
-			zip_t* newZipPtr = zip_open_from_source(
-				zipSrcPtr,
-				flags,
-				error.getInternalStructPtr()
-			);
+					throw std::runtime_error(
+						"cannot create a zip archive source -> "
+							+ error.getErrMessage()
+					);
 
-			if (!newZipPtr) {
+				}
 
-				zip_source_free(zipSrcPtr);
-
-				throw std::runtime_error(
-					std::string("cannot open a zip archive from the data source -> ")
-						+ error.getErrMessage()
+				zip_t* newZipPtr = zip_open_from_source(
+					zipSrcPtr,
+					ZIP_CREATE,
+					error.getInternalStructPtr()
 				);
 
+				if (!newZipPtr) {
+
+					zip_source_free(zipSrcPtr);
+
+					throw std::runtime_error(
+						"cannot open a zip archive from the data source -> "
+							+ error.getErrMessage()
+					);
+
+				}
+
+				return std::make_shared<ZipHandle>(newZipPtr, writableSource);
 			}
-
-			return std::make_shared<ZipHandle>(newZipPtr, writableSource);
-		}
-
-	private:
-
-		OutputStream _outputStreamPtr;
-
-	};
-
-	// create an instance of output archive stream
-	template<typename OutputStream>
-	static OutputArchiveStream<
-		OutputStream
-	> MakeOutputArchive(OutputStream os)
-	{
-		return OutputArchiveStream<OutputStream>(os);
-	}
-
-	// create a shared pointer to output archive stream
-	template<typename OutputStream>
-	static std::shared_ptr<
-		OutputArchiveStream<OutputStream>
-	> MakeSharedOutputArchive(OutputStream os)
-	{
-		return std::make_shared<
-			OutputArchiveStream<OutputStream>
-		>(os);
+		);
 	}
 
 }
